447631da316da36cdaa06c1a3e3c79f0c87a155c
Amar Tech sutra <amartechsutra@gmail.com>
2025-08-13 08:45:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAxRuiDaa3gENn6-85Naol3kmpayFZ6heQ",
+  authDomain: "stare-6545e.firebaseapp.com",
+  projectId: "stare-6545e",
+  storageBucket: "stare-6545e.firebasestorage.app",
+  messagingSenderId: "650083270679",
+  appId: "1:650083270679:web:cab7fcd3cedffd247cdc52",
+  measurementId: "G-DEZ0GYDPWM"
 };
 
 

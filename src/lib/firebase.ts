798a5b0d80ead78675ebfd191016b273727c842b
Amar Tech sutra <amartechsutra@gmail.com>
2025-08-13 08:48:11 +0000

    Console Error
    FirebaseError: Firebase: Error (auth/configuration-not-f

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAxRuiDaa3gENn6-85Naol3kmpayFZ6heQ",
   authDomain: "stare-6545e.firebaseapp.com",
   projectId: "stare-6545e",
-  storageBucket: "stare-6545e.firebasestorage.app",
+  storageBucket: "stare-6545e.appspot.com",
   messagingSenderId: "650083270679",
   appId: "1:650083270679:web:cab7fcd3cedffd247cdc52",
   measurementId: "G-DEZ0GYDPWM"
@@ -18,12 +16,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0];
-}
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);

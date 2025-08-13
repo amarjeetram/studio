dc452e40f0bff69b9c56e6247fdb2e67b388fe16
Amar Tech sutra <amartechsutra@gmail.com>
2025-08-13 08:53:19 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).
    src/app/

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);

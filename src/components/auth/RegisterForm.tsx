936d71f461306748457569d0c7fed75fcdb13217
Amar Tech sutra <amartechsutra@gmail.com>
2025-09-29 15:11:06 +0000

    FirebaseError: Firebase: Error (auth/email-already-in-use).     at creat

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -81,10 +81,6 @@ export function RegisterForm() {
           message: `Registration Failed: ${error.message || "An unexpected error occurred."}`,
         });
       }
-    } finally {
-        if(form.formState.isSubmitSuccessful === false) {
-            form.reset(form.getValues());
-        }
     }
   }
 
